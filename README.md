48b8323d08d5124b7c6ca4d99c9b8c074cb311ee
Wonder Diaz <wonderdxk@gmail.com>
2019-02-16 22:54:24 -0400

    1.0.2
    - Se eliminaron librerias sin utilidad
    - Se agregaron las tasas de Euros
    - Se agregaron botones para compartir las tasas (sin activar)
    - Se cambio el nombre del proyecto
    - Se agregaron dos cuadros para mostrar el promedio en USD/EUR

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Monitor App **Alpha**
 Derechos de Autor (C) 2019 Wonder Diaz
 
-Esta aplicacion es un software de codigo abierto para la verificacion de tasas del dolar paralelo en Venezuela. Estara consumiendo distintos datos para mejorar la experiencia.
+Esta aplicacion es un software de codigo abierto para la verificacion de tasas del dolar paralelo en Venezuela. Estara consumiendo distintos datos para mejorar la ncia.
 
+> Version 1.0.2
+- Se eliminaron librerias sin utilidad
+- Se agregaron las tasas de Euros
+- Se agregaron botones para compartir las tasas (sin activar)
+- Se cambio el nombre del proyecto
+- Se agregaron dos cuadros para mostrar el promedio en USD/EUR
 > Version 1.0.1
 - Inicio del Proyecto
 - Mejora en el diseño y presentacion de datos
--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
-$.getJSON("https://s3.amazonaws.com/dolartoday/data.json", function(data) {
+//Dolar Today
+$.getJSON("https://s3.amazonaws.com/dolartoday/data.json", function (data) {
   //Fecha
-  $("#Fecha").html("DolarToday al: " + data._timestamp.fecha);
-  //Tasas
-  $("#transferencia").html("Transferencia: " + data.USD.transferencia + "<br>");
-  $("#efectivo_real").html("Efectivo: " + data.USD.efectivo_real + "<br>");
-  $("#promedio_real").html("Promedio: " + data.USD.promedio_real + "<br>");
-  $("#bitcoin_ref").html("Bitcoin: " + data.USD.bitcoin_ref + "<br>");
-  $("#sicad1").html("Sicad: " + data.USD.sicad1 + "<br>");
-  $("#dolartoday").html("Dolartoday: " + data.USD.dolartoday + "<br>");
+  $("#Fecha").html("Fecha: " + data._timestamp.fecha);
+  //Tasas en USD
+  $("#transferenciaUSD").html("Transferencia: " + data.USD.transferencia + "<br>");
+  $("#efectivo_realUSD").html("Efectivo: " + data.USD.efectivo_real + "<br>");
+  $("#promedio_realUSD").html("Promedio: " + data.USD.promedio_real + "<br>");
+  $("#bitcoin_refUSD").html("Bitcoin: " + data.USD.bitcoin_ref + "<br>");
+  $("#sicad1USD").html("Sicad: " + data.USD.sicad1 + "<br>");
+  $("#dolartodayUSD").html("Dolartoday: " + data.USD.dolartoday + "<br>");
+ //Tasas en EUR
+ $("#transferenciaEUR").html("Transferencia: " + data.EUR.transferencia + "<br>");
+ $("#efectivo_realEUR").html("Efectivo: " + data.EUR.efectivo_real + "<br>");
+ $("#promedio_realEUR").html("Promedio: " + data.EUR.promedio_real + "<br>");
+ $("#sicad1EUR").html("Sicad: " + data.EUR.sicad1 + "<br>");
+ $("#dolartodayEUR").html("Dolartoday: " + data.EUR.dolartoday + "<br>");
 });
