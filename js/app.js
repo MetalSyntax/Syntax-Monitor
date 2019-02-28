42aa440f002beda86644e087c6b0a4084c9dcf9c
Wonder Diaz <wonderdxk@gmail.com>
2019-02-27 22:44:54 -0400

    1.0.3
    Se agregaron las fuentes WOFF, TTF, SVG, OET

--- js/app.js
+++ js/app.js
@@ -9,16 +9,26 @@ $.getJSON("https://s3.amazonaws.com/dolartoday/data.json", function (data) {
   $("#bitcoin_refUSD").html("Bitcoin: " + data.USD.bitcoin_ref + "<br>");
   $("#sicad1USD").html("Sicad: " + data.USD.sicad1 + "<br>");
   $("#dolartodayUSD").html("Dolartoday: " + data.USD.dolartoday + "<br>");
- //Tasas en EUR
- $("#transferenciaEUR").html("Transferencia: " + data.EUR.transferencia + "<br>");
- $("#efectivo_realEUR").html("Efectivo: " + data.EUR.efectivo_real + "<br>");
- $("#promedio_realEUR").html("Promedio: " + data.EUR.promedio_real + "<br>");
- $("#sicad1EUR").html("Sicad: " + data.EUR.sicad1 + "<br>");
- $("#dolartodayEUR").html("Dolartoday: " + data.EUR.dolartoday + "<br>");
- //
+  //Tasas en EUR
+  $("#transferenciaEUR").html("Transferencia: " + data.EUR.transferencia + "<br>");
+  $("#efectivo_realEUR").html("Efectivo: " + data.EUR.efectivo_real + "<br>");
+  $("#promedio_realEUR").html("Promedio: " + data.EUR.promedio_real + "<br>");
+  $("#sicad1EUR").html("Sicad: " + data.EUR.sicad1 + "<br>");
+  $("#dolartodayEUR").html("Dolartoday: " + data.EUR.dolartoday + "<br>");
+  //
   $("#promedioUSD").html("Promedio: " + data.USD.promedio_real + "<br>");
   $("#promedioEUR").html("Promedio: " + data.EUR.promedio_real + "<br>");
 });
 
+//Yadio
+$.getJSON("https://api.yadio.io/json", function (dataYadio) {
+  $("#YadioUSD").html("Dolares" + dataYadio.USD.rate + "<br>");
+  console.log(dataYadio);
+});
+
+
+//WhatsApp
+$.getJSON("https://s3.amazonaws.com/dolartoday/data.json", function (data) {
   var mensaje = "Promedio de Dolar:" + data.USD.promedio_real + '<br>' + "Promedio de Euro:" + data.EUR.promedio_real;
-  var url = "whatsapp://send?text="+ encodeURIComponent(mensaje);
+  var url = "whatsapp://send?text=" + encodeURIComponent(mensaje);
+});
