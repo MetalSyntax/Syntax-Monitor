8202e3f1b04f6063918a819b57219cf8ca79313c
Wonder Diaz <wonderdxk@gmail.com>
2019-03-03 00:34:15 -0400

    1.0.5
    - Calculadora sin funcionar
    - Tasas de Yadio Activas
    - Se elimino el codigo Jquery
    - Se mejoro la estructura HTML

--- js/app.js
+++ js/app.js
@@ -1,39 +1,48 @@
 //Dolar Today
-$.getJSON("https://s3.amazonaws.com/dolartoday/data.json", function(data) {
+fetch('https://s3.amazonaws.com/dolartoday/data.json').then(function (response) {
+  return response.json();
+}).then(function (data) {
+  
   //Fecha
-  $("#Fecha").html("Fecha: " + data._timestamp.fecha);
+  document.getElementById("Fecha").innerHTML = data._timestamp.fecha;
   //Tasas en USD
-  $("#transferenciaUSD").html(
-    "Transferencia: " + data.USD.transferencia + " VES <br>"
-  );
-  $("#efectivo_realUSD").html(
-    "Efectivo: " + data.USD.efectivo_real + " VES <br>"
-  );
-  $("#promedio_realUSD").html(
-    "Promedio: " + data.USD.promedio_real + " VES <br>"
-  );
-  $("#bitcoin_refUSD").html("Bitcoin: " + data.USD.bitcoin_ref + " VES <br>");
-  $("#sicad1USD").html("Sicad: " + data.USD.sicad1 + " VES <br>");
-  $("#dolartodayUSD").html("Dolartoday: " + data.USD.dolartoday + " VES <br>");
+  document.getElementById("transferenciaUSD").innerHTML = data.USD.transferencia;
+  document.getElementById("efectivo_realUSD").innerHTML = data.USD.efectivo_real;
+  document.getElementById("bitcoin_refUSD").innerHTML = data.USD.bitcoin_ref;
+  document.getElementById("dolartodayUSD").innerHTML = data.USD.dolartoday;
+  //Promedio Listado
+  document.getElementById("promedio_realUSD").innerHTML = data.USD.promedio_real;
+
   //Tasas en EUR
-  $("#transferenciaEUR").html(
-    "Transferencia: " + data.EUR.transferencia + " VES <br>"
-  );
-  $("#efectivo_realEUR").html(
-    "Efectivo: " + data.EUR.efectivo_real + " VES <br>"
-  );
-  $("#promedio_realEUR").html(
-    "Promedio: " + data.EUR.promedio_real + " VES <br>"
-  );
-  $("#sicad1EUR").html("Sicad: " + data.EUR.sicad1 + " VES <br>");
-  $("#dolartodayEUR").html("Dolartoday: " + data.EUR.dolartoday + " VES <br>");
-  //
-  $("#promedioUSD").html("Promedio: " + data.USD.promedio_real + " VES <br>");
-  $("#promedioEUR").html("Promedio: " + data.EUR.promedio_real + " VES <br>");
+  document.getElementById("transferenciaEUR").innerHTML = data.EUR.transferencia;
+  document.getElementById("efectivo_realEUR").innerHTML = data.EUR.efectivo_real;
+  document.getElementById("dolartodayEUR").innerHTML = data.EUR.dolartoday;
+  //Promedio Listado
+  document.getElementById("promedio_realEUR").innerHTML = data.EUR.promedio_real;
+
+  //Promedio Cuadro
+  document.getElementById("promedioUSD").innerHTML = data.USD.promedio_real;
+  document.getElementById("promedioEUR").innerHTML = data.EUR.promedio_real;
+}).catch(function (err) {
+  console.log("Hubo un Error: " + err);
 });
 
+//Yadio.io
+fetch('https://cors-anywhere.herokuapp.com/https://api.yadio.io/json').then(function (response) {
+  return response.json();
+}).then(function (data) {
+  document.getElementById("yadioUSD").innerHTML = data.USD.avg24h.toFixed(2);
+  document.getElementById("yadioEUR").innerHTML = data.rates.EUR.toFixed(2);
+}).catch(function (err) {
+  console.log("Hubo un Error: " + err);
+});
+
+//Dolar Satoshi
+var localbitcoin = 'https://cors-anywhere.herokuapp.com/https://localbitcoins.com/sell-bitcoins-online/vef/.json';
+var coinmarketcap ='https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=USD';
+
 //WhatsApp
-$.getJSON("https://s3.amazonaws.com/dolartoday/data.json", function(data) {
+$.getJSON("https://s3.amazonaws.com/dolartoday/data.json", function (data) {
   var mensaje =
     "Promedio de Dolar: " +
     data.USD.promedio_real +
@@ -53,7 +62,7 @@ $.getJSON("https://s3.amazonaws.com/dolartoday/data.json", function(data) {
 });
 
 //Twitter
-$.getJSON("https://s3.amazonaws.com/dolartoday/data.json", function(data) {
+$.getJSON("https://s3.amazonaws.com/dolartoday/data.json", function (data) {
   var mensaje =
     "Promedio de Dolar: " +
     data.USD.promedio_real +
@@ -66,7 +75,7 @@ $.getJSON("https://s3.amazonaws.com/dolartoday/data.json", function(data) {
     "https://twitter.com/intent/tweet?url=" +
     url +
     "&text=" +
-    mensaje + " " +  hora +
+    mensaje + " " + hora +
     "&hashtags=" +
     hashtags;
   $("#twitter").attr({
@@ -76,13 +85,13 @@ $.getJSON("https://s3.amazonaws.com/dolartoday/data.json", function(data) {
 });
 
 //Telegram
-$.getJSON("https://s3.amazonaws.com/dolartoday/data.json", function(data) {
+$.getJSON("https://s3.amazonaws.com/dolartoday/data.json", function (data) {
   var mensaje =
     "Promedio de Dolar: " +
     data.USD.promedio_real +
     " Promedio de Euro: " +
     data.EUR.promedio_real;
-      var hora = data._timestamp.fecha;
+  var hora = data._timestamp.fecha;
   var url = "https://metalsyntax.github.io/Syntax-Monitor/";
   var tele = "https://t.me/share/url?url=" + url + "&text=" + mensaje + " " + hora;
   $("#telegram").attr({
@@ -92,7 +101,7 @@ $.getJSON("https://s3.amazonaws.com/dolartoday/data.json", function(data) {
 });
 
 //Animacion
-var TxtRotate = function(el, toRotate, period) {
+var TxtRotate = function (el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
@@ -102,7 +111,7 @@ var TxtRotate = function(el, toRotate, period) {
   this.isDeleting = false;
 };
 
-TxtRotate.prototype.tick = function() {
+TxtRotate.prototype.tick = function () {
   var i = this.loopNum % this.toRotate.length;
   var fullTxt = this.toRotate[i];
 
@@ -130,12 +139,12 @@ TxtRotate.prototype.tick = function() {
     delta = 500;
   }
 
-  setTimeout(function() {
+  setTimeout(function () {
     that.tick();
   }, delta);
 };
 
-window.onload = function() {
+window.onload = function () {
   var elements = document.getElementsByClassName("txt-rotate");
   for (var i = 0; i < elements.length; i++) {
     var toRotate = elements[i].getAttribute("data-rotate");
@@ -151,22 +160,10 @@ window.onload = function() {
   document.body.appendChild(css);
 };
 
-//Yadio.io 
-$.getJSON("http://cors.io/?https://api.yadio.io/json", function(data) {
-  console.log(data.USD.avg24h);
-});
-
-//Experimental
-/*var requestURL = "http://cors.io/?https://api.yadio.io/json";
-var request = new XMLHttpRequest();
-request.open("GET", requestURL);
-request.responseType = "json";
-request.send();
-request.onload = function () {
-  var datos = request.response;
-  dolares(datos);
-};
-function dolares(jsonObj) {
-  var usd = document.getElementById("yadioUSD");
-  usd.textContent = jsonObj["USD"]["avg24h"];
-}*/
+//Refrescar Pagina
+function actualizar() {
+  location.reload(true);
+}
+setTimeout(function () {
+  actualizar();
+}, 900000);
